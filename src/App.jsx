cd88f6694dec33ca9899aae62be206dd2120d7ce
cyr rouyrre <you@example.com>
2025-03-05 10:15:37 +0100

    more img modifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Category from "./components/Category"
 import './App.css'
+import logo from "./img/logo-netflix.png"
 
 const App =()=>{ 
 return(<section className="body-box">
-<header><div><img src="src\img\logo-netflix.png" alt="not found" /></div></header>
+<header><div><img src= {logo} alt="not found" /></div></header>
 <main>
     <Category/>
 </main>
